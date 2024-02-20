2f9952723982d584c9baf4ff010db2016a80ded0
Ibrahim-Asiri <i-m-6@hotmail.com>
2024-02-20 19:32:51 +0300

    finally insert data into DB

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,7 +1,38 @@
 package com.example.demo.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
 
 @Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository ) {
+        return args -> {
+            Student ibrahim = new Student(
+                    "Ibrahim",
+                    "ibra@i.com",
+                    LocalDate.of(2002, FEBRUARY, 2),
+                    21
+            );
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "ahmed@a.com",
+                    LocalDate.of(2004, MAY, 18),
+                    20
+            );
+
+            repository.saveAll(
+                    List.of(ibrahim, ahmed)
+            );
+
+        };
+    }
 }
